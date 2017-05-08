a6bdbe3d1e1e9ed0f6875e467464ef88ae57895a
Maciej Żurad <maciej.zurad@gmail.com>
2017-05-08 14:41:41 +0200

    adding babel to support ES6/ES7

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,14 @@ var path = require('path');
 module.exports = {
   entry: './app/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
 };
