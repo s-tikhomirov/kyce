7f1fd3f1ad806e1714ee51a06d886c36b19939c3
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2017-05-08 14:00:07 +0200

    add ERC20 (needs more testing)

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -1,21 +1,122 @@
 pragma solidity ^0.4.11;
 
 
+contract ERC20Token {
+  
+     // Get the account balance of another account with address _owner
+     function balanceOf(address _owner) constant returns (uint256 balance);
+  
+     // Send _value amount of tokens to address _to
+     function transfer(address _to, uint256 _value) returns (bool success);
+  
+     // Send _value amount of tokens from address _from to address _to
+     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  
+     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+     // If this function is called again it overwrites the current allowance with _value.
+     // this function is required for some DEX functionality
+     function approve(address _spender, uint256 _value) returns (bool success);
+  
+     // Returns the amount which _spender is still allowed to withdraw from _owner
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  
+     // Triggered when tokens are transferred.
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  
+     // Triggered whenever approve(address _spender, uint256 _value) is called.
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+contract TestToken is ERC20Token {
+    
+    mapping(address => uint256) balances;
+   
+    // Owner of account approves the transfer of an amount to another account
+    mapping(address => mapping (address => uint256)) allowed;
+   
+    // Pre-allocale tokens to addresses hard-coded in the browser compiler
+    // https://ethereum.github.io/browser-solidity/
+    function TestToken() {
+        balances[0xca35b7d915458ef540ade6068dfe2f44e8fa733c] = 1000;
+        balances[0x14723a09acff6d2a60dcdf7aa4aff308fddc160c] = 1000;
+        balances[0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db] = 1000;
+        balances[0x583031d1113ad414f02576bd6afabfb302140225] = 1000;
+        balances[0xdd870fa1b7c4700f2bd7f44238821c26f7392148] = 1000;
+    }
+   
+    // What is the balance of a particular account?
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+   
+    // Transfer the balance from owner's account to another account
+    function transfer(address _to, uint256 _amount) returns (bool success) {
+        if (balances[msg.sender] >= _amount 
+        && _amount > 0
+        && balances[_to] + _amount > balances[_to]) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+   
+    // Send _value amount of tokens from address _from to address _to
+    // The transferFrom method is used for a withdraw workflow, allowing contracts to send
+    // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
+    // fees in sub-currencies; the command should fail unless the _from account has
+    // deliberately authorized the sender of the message via some mechanism; we propose
+    // these standardized APIs for approval:
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) returns (bool success) {
+        if (balances[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+            balances[_from] -= _amount;
+            allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(_from, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+  
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+        allowed[msg.sender][_spender] = _amount;
+        Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+  
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+}
+
+
+
 contract Exchange {
     // TODO: introduce Events
     
     address owner;
     modifier onlyOwner() { if (msg.sender != owner) throw; _; }
+    
     // set by owner, stops trades, deposits, withdrawals
     bool private emergency = false;
+    modifier noEmergency() { if (emergency) throw; _; }
     
     function Exchange() { owner = msg.sender; }
     
-    // TEST is a token for testing (can be deposited freely)
-    enum Token { TEST, USD, EUR, BTC, ETH }
-    
     mapping (address => uint) balance;
-    mapping (address => mapping(uint => uint)) tokenBalance;
     
     struct Order {
         address author;
@@ -29,16 +130,15 @@ contract Exchange {
         Order[] ask;
     }
     
-    // A token to a pair of its order books
-    mapping (uint => OrderBook) orderBook;
+    // An ERC20 token address to a pair of its order books
+    mapping (address => OrderBook) orderBook;
     
-    
-    function getTokenBalance(Token token) constant returns (uint) {
-        return tokenBalance[msg.sender][uint(token)];
+    function getMyBalance() constant returns (uint) {
+        return balance[msg.sender];
     }
     
-    function getbalance() constant returns (uint) {
-        return balance[msg.sender];
+    function getMyTokenBalance(address tokenAddress) constant returns (uint) {
+        return ERC20Token(tokenAddress).allowance(msg.sender, this);
     }
     
     function deposit() external payable {
@@ -46,20 +146,20 @@ contract Exchange {
         balance[msg.sender] += msg.value;
     }
     
-    function depositTestToken(uint _amount) {
-        tokenBalance[msg.sender][uint(Token.TEST)] += _amount;
+    function depositToken(address tokenAddress, uint amount) {
+        ERC20Token(tokenAddress).approve(this, amount);
     }
     
     // can only withdraw ether
-    function withdraw(uint amount) external {
-        if (emergency) throw;
+    function withdraw(uint amount) external noEmergency {
         if (balance[msg.sender] < amount) throw;
         if (msg.sender.send(amount)) {
             balance[msg.sender] -= amount;
         }
     }
     
-    function isCorrect(Token token, uint amount, uint price, bool isBid) 
+    // TODO: re-implement with ERC20
+    function isCorrect(address tokenAddress, uint amount, uint price, bool isBid) 
     internal
     returns (bool)
     {
@@ -67,7 +167,7 @@ contract Exchange {
         if (amount == 0 || price == 0) return false;
         
         // must have enough tokens to sell
-        if (!isBid && tokenBalance[msg.sender][uint(token)] < amount) return false;
+        if (!isBid && getMyTokenBalance(tokenAddress) < amount) return false;
         
         // must have enough ether to buy (at declared price)
         if (isBid && balance[msg.sender] < amount * price) return false;
@@ -75,37 +175,34 @@ contract Exchange {
         return true;
     }
     
-    function placeOrder(Token token, uint amount, uint price, bool isBid)
-    external
+    function placeOrder(address tokenAddress, uint amount, uint price, bool isBid)
+    external noEmergency
     //returns (uint orderId)
     {
-        if (emergency) throw;
-        if (!isCorrect(token, amount, price, isBid)) throw;
+        //if (!isCorrect(token, amount, price, isBid)) throw;
         Order memory order = Order({
             author: msg.sender,
             amount: amount,
             price: price
         });
-        isBid ? orderBook[uint(token)].bid.push(order) : orderBook[uint(token)].ask.push(order);
+        isBid ? orderBook[tokenAddress].bid.push(order) : orderBook[tokenAddress].ask.push(order);
     }
     
-    function editOrder(Token token, uint idx, uint _newAmount, uint _newPrice, bool isBid)
-    external
+    function editOrder(address tokenAddress, uint idx, uint _newAmount, uint _newPrice, bool isBid)
+    external noEmergency
     {
-        if (emergency) throw;
-        if (!isCorrect(token, _newAmount, _newPrice, isBid)) throw;
-        var book = isBid ? orderBook[uint(token)].bid : orderBook[uint(token)].ask;
+        if (!isCorrect(tokenAddress, _newAmount, _newPrice, isBid)) throw;
+        var book = isBid ? orderBook[tokenAddress].bid : orderBook[tokenAddress].ask;
         if (msg.sender != book[idx].author) throw;
         book[idx].amount = _newAmount;
         book[idx].price = _newPrice;
         return;
     }
     
-    function deleteOrder(Token token, uint idx, bool isBid) 
-    external
+    function deleteOrder(address tokenAddress, uint idx, bool isBid) 
+    external noEmergency
     {
-        if (emergency) throw;
-        var book = isBid ? orderBook[uint(token)].bid : orderBook[uint(token)].ask;
+        var book = isBid ? orderBook[tokenAddress].bid : orderBook[tokenAddress].ask;
         if (msg.sender != book[idx].author) throw;
         book[idx] = book[book.length-1];
         book.length--;
@@ -126,12 +223,12 @@ contract Exchange {
         return (bid.amount == ask.amount) ? bid.amount : 0;
     }
     
-    function matchOrders(Token token)
-    //TODO: internal
+    function matchOrders(address tokenAddress)
+    noEmergency //TODO: internal
     returns (bool didMatch, uint _agreedPrice) {
-        if (emergency) throw;
-        var bidBook = orderBook[uint(token)].bid;
-        var askBook = orderBook[uint(token)].ask;
+        
+        var bidBook = orderBook[tokenAddress].bid;
+        var askBook = orderBook[tokenAddress].ask;
         
         if ( bidBook.length == 0 || askBook.length == 0 )
             return (false, 0);   // uint value has no meaning here
@@ -149,9 +246,10 @@ contract Exchange {
         if ( agreedPrice == 0 ) {
             return (false, 0);
         } else {
-            tokenBalance[bestBid.author][uint(token)] += agreedAmount;
+            if(!ERC20Token(tokenAddress).transferFrom(bestAsk.author, bestBid.author, agreedAmount))
+                throw;
+            
             balance[bestBid.author] -= agreedAmount * agreedPrice;
-            tokenBalance[bestAsk.author][uint(token)] -= agreedAmount;
             balance[bestAsk.author] += agreedAmount * agreedPrice;
             
             askBook[uint(bestAskIdx)] = askBook[askBook.length-1];
@@ -191,19 +289,19 @@ contract Exchange {
     
     /***** HELPER FUNCTIONS *****/
     
-    function getStats(Token token)
+    function getStats(address tokenAddress)
     constant
     onlyOwner
     returns (uint numberOfAsks, uint numberOfBids) {
-        return (orderBook[uint(token)].ask.length, orderBook[uint(token)].bid.length);
+        return (orderBook[tokenAddress].ask.length, orderBook[tokenAddress].bid.length);
     }
     
-    function getOrderInfo(Token token, uint idx, bool isBid)
+    function getOrderInfo(address tokenAddress, uint idx, bool isBid)
     constant
     onlyOwner
     returns (address author, uint amount, uint price)
     {
-        Order order = (isBid ? orderBook[uint(token)].bid : orderBook[uint(token)].ask)[idx];
+        Order order = (isBid ? orderBook[tokenAddress].bid : orderBook[tokenAddress].ask)[idx];
         return (order.author, order.amount, order.price);
     }
     
