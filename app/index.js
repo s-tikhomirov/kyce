6428cbeb240de0d29ba3c3779206506eb0964338
Maciej Żurad <maciej.zurad@gmail.com>
2017-05-08 14:27:25 +0200

    sample web3 example working with webpack

--- app/index.js
+++ app/index.js
@@ -1,11 +1,35 @@
-const _ = require('lodash');
+_ = require('lodash');
+Web3 = require('web3');
+web3 = new Web3();
+Exchange = require('../contracts/bin/exchange.js').Exchange;
+exchange = null;
+
+const isNodeConnected = () => web3.isConnected();
+
+function connectToEthereumNode(url) {
+  const provider = new web3.providers.HttpProvider(url);
+  web3.setProvider(provider);
+  console.log(web3);
+  if (isNodeConnected() === true) {
+    exchange = Exchange(web3);
+    web3.eth.defaultAccount = web3.eth.coinbase;
+    return true;
+  }
+  return false;
+}
 
 function component () {
   var element = document.createElement('div');
 
+  console.log(connectToEthereumNode('http://127.0.0.1:8080'));
+
+  console.log(web3);
+
   /* lodash is required for the next line to work */
   element.innerHTML = _.join(['Hello','webpack'], ' ');
-
+  element.innerHTML = _.join(
+    [element.innerHTML, `Exchange address is ${exchange.address}`]
+  );
   return element;
 }
 
