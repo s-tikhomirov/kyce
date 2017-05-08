24e7c2436a38f4ae374de9a41afb2dff8189a566
khovratovich <khovratovich@gmail.com>
2017-05-08 12:26:49 +0200

    Simple exchange added

---
+++ exchange-simple.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.10;
+
+
+contract ERC20Token{
+    function transferFrom (address _from, address _to, uint256 _value)
+    returns (bool success);
+    
+      function transfer (address _to, uint256 _value)
+    returns (bool success);
+    
+    function allowance(address _owner, address _spender)
+    constant
+    returns (uint256 remaining);
+    
+    function balanceOf (address _owner) constant returns (uint256 balance);
+    
+}
+
+contract Exchange{
+    
+    struct Order{
+        address tokenContract;
+        uint tokens;
+        uint value;
+        address sender;
+        bool isBid; //1 if bid 0 if ask
+        uint index; //internal indexing
+    }
+    
+    mapping (bytes32 => Order) orders;
+    bytes32[] ordersIndex;
+    
+    event OrderPlaced(address indexed _token, uint _tokens, uint _value, 
+        address indexed _sender, bool isBid, uint _index, bytes32 _id);
+    
+    //Want to buy _tokens of type  _tokenContract for msg.value
+    function ask(address _tokenContract, uint _tokens) payable returns (bytes32){
+        bytes32 id  = sha3(block.number, msg.data);
+        orders[id] = 
+            Order(_tokenContract,  _tokens, msg.value, msg.sender,false, ordersIndex.length++);
+        ordersIndex[orders[id].index] = id;
+        OrderPlaced(orders[id].tokenContract, orders[id].tokens, orders[id].value, 
+        orders[id].sender, orders[id].isBid, orders[id].index, id);
+        return id;
+    }
+    
+    //Want to sell _tokens of  type  _tokenContract for value
+    function bid(address _tokenContract, uint _tokens, uint _value) returns (bytes32){
+        bytes32 id = sha3(block.number, msg.data);
+        orders[id] = 
+            Order(_tokenContract,  _tokens, _value, msg.sender,true, ordersIndex.length++);
+        ordersIndex[orders[id].index] = id;
+        OrderPlaced(orders[id].tokenContract, orders[id].tokens, orders[id].value, 
+        orders[id].sender, orders[id].isBid, orders[id].index, id);
+        return id;
+    }
+    
+    function getOrder(uint _index) constant returns (address _tokenContract,
+        uint _tokens,  uint _value, address _sender, bool _isBid, bytes32 _id)
+        {
+            return (orders[ordersIndex[_index]].tokenContract, orders[ordersIndex[_index]].tokens,
+            orders[ordersIndex[_index]].value,orders[ordersIndex[_index]].sender,
+            orders[ordersIndex[_index]].isBid, ordersIndex[_index]);
+        }
+    
+	//Execute a bid or an ask
+    function execute(bytes32 id) payable returns (bool){
+        if(orders[id].tokenContract==0) throw;
+        ERC20Token token = ERC20Token(orders[id].tokenContract);
+        if(orders[id].isBid){  //it is bid
+            if(orders[id].value != msg.value)
+                throw;
+            if(!token.transferFrom(orders[id].sender, msg.sender,orders[id].tokens))
+                throw;
+            if(!orders[id].sender.send(orders[id].value))
+                throw;
+        }
+        else { //it is ask
+            if(msg.value!=0)  //do not accept Ether here
+                throw;
+            if(!token.transferFrom(msg.sender, orders[id].sender, orders[id].tokens))
+                throw;
+            if(!msg.sender.send(orders[id].value))
+                throw;
+        }
+        
+        //removing order
+        if(orders[id].index == ordersIndex.length-1){//last one
+            ordersIndex.length--;
+            delete orders[id];
+        }
+        else{
+             ordersIndex[orders[id].index] = ordersIndex[ordersIndex.length-1];
+             orders[ordersIndex[orders[id].index]].index = orders[id].index;
+             ordersIndex.length--;
+            delete orders[id];
+        }
+    }
+}
+
